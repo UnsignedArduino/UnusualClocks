555432986ef603ba0154dbd0b399c12c58013b88
Ckyiu <38868705+UnsignedArduino@users.noreply.github.com>
2021-12-14 13:28:31 +0000

    Template for p5.js

---
+++ script.js
@@ -0,0 +1,13 @@
+let width;
+let height;
+
+function setup() {
+  // Make it fit nicely on screen without scrolling
+  width = windowWidth - 20;
+  height = windowHeight - 25;
+  createCanvas(width, height);
+}
+
+function draw() {
+  background(240);
+}
